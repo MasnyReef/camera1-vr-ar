a85d700b7ab68f050643736fcc52261b450ef78a
Piotr2137 <piotrrachx@gmail.com>
2023-05-07 00:33:29 +0200

    OpenCV added

---
+++ openCV/build/intermediates/cxx/Debug/5i631z66/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\piotr\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\piotr\\AndroidStudioProjects\\mycamera1\\openCV\\.cxx\\Debug\\5i631z66\\arm64-v8a" ^
+  opencv_jni_shared
