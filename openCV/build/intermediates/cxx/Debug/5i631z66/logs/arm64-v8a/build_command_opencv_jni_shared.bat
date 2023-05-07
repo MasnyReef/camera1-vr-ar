0a0a067d13cba55b69c718e6865b9a8450dc0504
Piotr2137 <piotrrachx@gmail.com>
2023-05-07 14:30:59 +0200

    new branch

--- openCV/build/intermediates/cxx/Debug/5i631z66/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++ openCV/build/intermediates/cxx/Debug/5i631z66/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -1,5 +1,5 @@
 @echo off
-"C:\\Users\\Kacper\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+"C:\\Users\\piotr\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
   -C ^
-  "C:\\Users\\Kacper\\Documents\\GitHub\\camera1-vr-ar\\openCV\\.cxx\\Debug\\5i631z66\\arm64-v8a" ^
+  "C:\\Users\\piotr\\AndroidStudioProjects\\mycamera1\\openCV\\.cxx\\Debug\\5i631z66\\arm64-v8a" ^
   opencv_jni_shared
